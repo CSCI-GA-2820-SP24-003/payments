39b2eef62f9b18fd27cae772198fec17869c84cc
Sean Mo <94581000+bairixie@users.noreply.github.com>
2024-03-01 22:10:41 -0500

    Update factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -28,11 +28,7 @@ class PaymentFactory(factory.Factory):
     class Meta:  # pylint: disable=too-few-public-methods
         """Maps factory to data model"""
 
-        model = Pet
+        model = PaymentMethod
 
     id = factory.Sequence(lambda n: n)
     name = factory.Faker("first_name")
-    category = FuzzyChoice(choices=["dog", "cat", "bird", "fish"])
-    available = FuzzyChoice(choices=[True, False])
-    gender = FuzzyChoice(choices=[Gender.MALE, Gender.FEMALE, Gender.UNKNOWN])
-    birthday = FuzzyDate(date(2008, 1, 1))
