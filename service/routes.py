08c0e8a3c2610e61c0367e6b854022f40b9340be
Sean Mo <94581000+bairixie@users.noreply.github.com>
2024-03-03 21:28:04 -0500

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -27,17 +27,6 @@ from service.models import PaymentMethod
 from service.common import status  # HTTP Status Codes
 
 
-######################################################################
-# GET INDEX
-######################################################################
-@app.route("/")
-def index():
-    """Root URL response"""
-    return (
-        "Reminder: return some useful information in json format about the service here",
-        status.HTTP_200_OK,
-    )
-
 
 ######################################################################
 #  R E S T   A P I   E N D P O I N T S
