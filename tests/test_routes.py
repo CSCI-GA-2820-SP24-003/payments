6e819dfb1f91de6f6359c751f55a3f8a3c13fbd9
yiyan <yn2161@nyu.edu>
2024-04-17 04:15:13 +0000

    Fix test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -411,5 +411,5 @@ class TestPaymentsService(TestCase):
         self.assertTrue(resp2_updated.get_json()["is_default"])
 
     def test_generate_apikey(self):
-        """It should Generate a string of length 16"""
-        self.assertEqual(len(generate_apikey()), 16)
+        """It should Generate a string of length 32"""
+        self.assertEqual(len(generate_apikey()), 32)
