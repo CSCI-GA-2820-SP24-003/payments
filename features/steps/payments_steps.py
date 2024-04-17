d3eaece5945eefcb0e797cb09ba8e094c50f5311
yiyan <yn2161@nyu.edu>
2024-04-17 02:03:35 +0000

    Update base url for bdd

--- features/steps/payments_steps.py
+++ features/steps/payments_steps.py
@@ -16,7 +16,7 @@ def step_impl(context):
     """ Delete all payment methods and load new ones """
 
     # List all of the payment methods and delete them one by one
-    rest_endpoint = f"{context.base_url}/payments"
+    rest_endpoint = f"{context.base_url}/api/payments"
     context.resp = requests.get(rest_endpoint)
     assert context.resp.status_code == HTTP_200_OK
     for payment_method in context.resp.json():
