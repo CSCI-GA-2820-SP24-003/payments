206071ff472395f0bbd2d2a5bd174dcaeb9fe8cc
yiyan <yn2161@nyu.edu>
2024-04-17 18:39:35 +0000

    Add all errors back

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -83,3 +83,18 @@ def mediatype_not_supported(error):
         ),
         status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
     )
+
+
+@app.errorhandler(status.HTTP_500_INTERNAL_SERVER_ERROR)
+def internal_server_error(error):
+    """Handles unexpected server error with 500_SERVER_ERROR"""
+    message = str(error)
+    app.logger.error(message)
+    return (
+        jsonify(
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            error="Internal Server Error",
+            message=message,
+        ),
+        status.HTTP_500_INTERNAL_SERVER_ERROR,
+    )
